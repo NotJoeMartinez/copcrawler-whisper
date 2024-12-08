e9adb29a068bc8530d16e0b0a78446d54f74e0f0
Joe Martinez <notjoemartinez@tutanota.com>
2024-12-08 00:42:55 -0600

    benchmark against tiny and tiny.en

--- inference.py
+++ inference.py
@@ -3,40 +3,75 @@ import argparse
 
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '--model',
-    required=True
-)
-parser.add_argument(
-    '--input',
-    required=True
-)
-args = parser.parse_args()
+from faster_whisper import WhisperModel
+from multiprocessing import cpu_count
 
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--model',
+        required=True
+    )
+    parser.add_argument(
+        '--input',
+        required=True
+    )
+    args = parser.parse_args()
 
-model_id = args.model
+    if args.model == 'tiny':
+        run_tiny_model(args.input)
 
-model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, 
-    low_cpu_mem_usage=True, 
-    use_safetensors=True
-)
-model.to(device)
 
-processor = AutoProcessor.from_pretrained(model_id)
+def run_tiny_model(file_path):
+    model = WhisperModel('tiny', 
+                        device="cpu", 
+                        compute_type="int8", 
+                        num_workers=8,
+                        cpu_threads=cpu_count())
 
-pipe = pipeline(
-    'automatic-speech-recognition',
-    model=model,
-    tokenizer=processor.tokenizer,
-    feature_extractor=processor.feature_extractor,
-    torch_dtype=torch_dtype,
-    device=device
-)
+    segments, info = model.transcribe(file_path, 
+                     beam_size=5,
+                     condition_on_previous_text=False,
+                     language="en",
+                     no_speech_threshold=1.5, 
+                     repetition_penalty=1.7)
+    
+    output = ""
 
-result = pipe(args.input, generate_kwargs={'task': 'transcribe'})
+    for segment in segments:
+        output += segment.text + " "
+    
+    print(output)
+    
+def run_fine_tuned_model(args):
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-print('\n', result)
+    model_id = args.model
+
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, 
+        low_cpu_mem_usage=True, 
+        use_safetensors=True
+    )
+    model.to(device)
+
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    pipe = pipeline(
+        'automatic-speech-recognition',
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        torch_dtype=torch_dtype,
+        device=device
+    )
+
+    result = pipe(args.input, generate_kwargs={'task': 'transcribe'})
+    text = result['text']
+    print(text)
+
+
+
+if __name__ == '__main__':
+    main()
